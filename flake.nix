df6343fe0a613f18bdf3454bf60c94e98a51adce
Romain Deville <code@romaindeville.fr>
2024-06-24 00:22:19 +0200

    🔨 Add flake.nix and flake.lock

--- flake.nix
+++ flake.nix
@@ -1,69 +1,100 @@
+# BEGIN DOTGIT-SYNC BLOCK MANAGED
 {
-  # Inspiration sources : https://github.com/jonringer/nixpkgs-config.git
-  description = "My NixOS and Home Manager configuration";
+  description = ''
+    Flake for Nixos Config Data
+
+    Repository storing configuration for my computer and users used with NixOS and
+    Home-Manager
+  '';
+  # Devenv Cachix
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
 
   inputs = {
     # Stable Nix Packages
     nixpkgs = {
-      url = "nixpkgs/nixos-24.05";
+      # url = "github:nixos/nixpkgs/nixos-24.05";
+      url = "github:nixos/nixpkgs/nixos-unstable";
     };
     # Flake Utils Lib
     utils = {
       url = "github:numtide/flake-utils";
     };
-
     # Devenv to automate development environment combined with direnv
     devenv = {
       url = "github:cachix/devenv";
     };
-    # # Nix formatter
+    # Nix formatter
     alejandra = {
       url = "github:kamadorueda/alejandra/3.0.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
+    # BEGIN DOTGIT-SYNC BLOCK EXCLUDED NIX_FLAKE_INPUT
+    # Home Manager, manage your Home from nix
+    home-manager = {
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+      url = "github:nix-community/home-manager";
+    };
+    # Unstable Nix Packages
+    nixpkgs-unstable = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
     # NixOS /HM Config
     nixos = {
-      url = "/home/rdeville/git/framagit.org/public/dotfiles/nixos";
+      url = "git+https://framagit.org/rdeville-public/dotfiles/nixos-config.git";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        utils.follows = "utils";
+        home-manager.follows = "home-manager";
+        nixpkgs-unstable.follows = "nixpkgs-unstable";
+      };
+    };
+    # My personal dotfiles flakes
+    awesomerc = {
+      url = "git+https://framagit.org/rdeville-public/dotfiles/awesome.git";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.utils.follows = "utils";
     };
-    # My personal dotfiles flakes
-    direnv = {
+    direnvrc = {
       url = "git+https://framagit.org/rdeville-public/dotfiles/direnv.git";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.utils.follows = "utils";
     };
+    neovimrc = {
+      url = "git+https://framagit.org/rdeville-public/dotfiles/neovim.git";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.utils.follows = "utils";
+    };
+    tmuxrc = {
+      url = "git+https://framagit.org/rdeville-public/dotfiles/tmux.git";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.utils.follows = "utils";
+    };
+    zshrc = {
+      url = "git+https://framagit.org/rdeville-public/dotfiles/shell.git";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.utils.follows = "utils";
+    };
+    # My programs I want on my computer
+    dotgit-sync = {
+      url = "git+https://framagit.org/rdeville-public/programs/dotgit-sync.git";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.utils.follows = "utils";
+    };
+    # END DOTGIT-SYNC BLOCK EXCLUDED NIX_FLAKE_INPUT
   };
 
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    utils,
-    devenv,
-    alejandra,
-    nixos,
-    direnv,
-    ...
-  }: let
-    # All my hosts with users configs
-    allConfigs = import ./configs {
-      mkLib = nixos.homeManagerModules.accountLib;
-      inherit direnv;
-    };
-
+  outputs = inputs @ {self, ...}: let
     pkgsForSystem = system:
-      import nixpkgs {
+      import inputs.nixpkgs {
         inherit system;
       };
 
     # This is a function that generates an attribute by calling a function you
     # pass to it, with each system as an argument
-    forAllSystems = nixpkgs.lib.genAttrs allSystems;
-
-    hmLib = nixos.homeManagerModules.hmLib {inherit (nixos) inputs;};
-    nixosLib = nixos.homeManagerModules.nixosLib {inherit (nixos) inputs;};
-    mkLib = nixos.homeManagerModules.mkLib {inherit (nixos) inputs;};
+    forAllSystems = inputs.nixpkgs.lib.genAttrs allSystems;
 
     allSystems = [
       "x86_64-linux"
@@ -71,17 +102,28 @@
       "x86_64-darwin"
       "aarch64-darwin"
     ];
+
+    # BEGIN DOTGIT-SYNC BLOCK EXCLUDED NIX_FLAKE_CUSTOM_VARS
+    allConfigs = import ./configs {
+      mkLib = inputs.nixos.homeManagerModules.accountLib;
+      inherit inputs;
+    };
+
+    hmLib = inputs.nixos.homeManagerModules.hmLib {inherit (inputs.nixos) inputs;};
+    nixosLib = inputs.nixos.homeManagerModules.nixosLib {inherit (inputs.nixos) inputs;};
+    # mkLib = inputs.nixos.homeManagerModules.mkLib {inherit (inputs.nixos) inputs;};
+    # END DOTGIT-SYNC BLOCK EXCLUDED NIX_FLAKE_CUSTOM_VARS
   in
-    utils.lib.eachSystem allSystems (
+    inputs.utils.lib.eachSystem allSystems (
       system: let
         pkgs = pkgsForSystem system;
-      in rec {
+      in {
         packages = {
           devenv-up = self.devShells.${system}.default.config.procfileScript;
         };
 
         devShells = {
-          default = devenv.lib.mkShell {
+          default = inputs.devenv.lib.mkShell {
             inherit inputs pkgs;
             modules = [
               ./devenv.nix
@@ -97,9 +139,10 @@
       # Other options beside 'alejandra' include 'nixpkgs-fmt'
       formatter = forAllSystems (
         system:
-          alejandra.defaultPackage.${system}
+          inputs.alejandra.defaultPackage.${system}
       );
 
+      # BEGIN DOTGIT-SYNC BLOCK EXCLUDED NIX_FLAKE_OUTPUTS_CUSTOM
       # CONFIGURATION
       # ========================================================================
       # NIXOS
@@ -112,12 +155,12 @@
       # VMs I use to test NixOS configs
       # Build NixOS VMs using nixos-shell, see nixvm.sh to deploy
       nixos-shellConfigurations = builtins.mapAttrs (name: value:
-        nixosLib.mkNixOS (nixosLib.mkNixVMs-nixos-shell value))
+        nixosLib.mkNixOS (inputs.nixosLib.mkNixVMs-nixos-shell value))
       allConfigs.vms;
 
       # Build NixOS VMs using microvm, see nixvm.sh to deploy
       microvmConfigurations = builtins.mapAttrs (name: value:
-        nixosLib.mkNixOS (nixosLib.mkNixVMs-microvm value))
+        nixosLib.mkNixOS (inputs.nixosLib.mkNixVMs-microvm value))
       allConfigs.vms;
 
       # HOME MANAGER
@@ -126,5 +169,7 @@
       homeConfigurations = builtins.mapAttrs (name: value:
         hmLib.mkHomeConfiguration value)
       (hmLib.mkHomeConfigs allConfigs);
+      # END DOTGIT-SYNC BLOCK EXCLUDED NIX_FLAKE_OUTPUTS_CUSTOM
     };
 }
+# END DOTGIT-SYNC BLOCK MANAGED
