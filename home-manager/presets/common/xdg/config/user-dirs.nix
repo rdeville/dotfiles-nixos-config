7ec5aac5f13ab2b76aa6402a6026243218dcd759
Romain Deville <code@romaindeville.fr>
2024-07-08 13:35:08 +0200

    🐛(presets): Fix user-dirs in preset common

--- home-manager/presets/common/xdg/config/user-dirs.nix
+++ home-manager/presets/common/xdg/config/user-dirs.nix
@@ -9,15 +9,15 @@
       # I usually have only three home folder: cloud, git and downloads
       createDirectories = false;
       enable = false;
-      desktop = "${config.home.homeDirectory}/"
-      documents = "${config.home.homeDirectory}/"
-      download = "${config.home.homeDirectory}/downloads"
-      extraConfig = "${config.home.homeDirectory}/"
-      music = "${config.home.homeDirectory}/"
-      pictures = "${config.home.homeDirectory}/"
-      publicShare = "${config.home.homeDirectory}/public"
-      templates = "${config.home.homeDirectory}/"
-      videos = "${config.home.homeDirectory}/"
+      desktop = "${config.home.homeDirectory}/";
+      documents = "${config.home.homeDirectory}/";
+      download = "${config.home.homeDirectory}/downloads";
+      extraConfig = "${config.home.homeDirectory}/";
+      music = "${config.home.homeDirectory}/";
+      pictures = "${config.home.homeDirectory}/";
+      publicShare = "${config.home.homeDirectory}/public";
+      templates = "${config.home.homeDirectory}/";
+      videos = "${config.home.homeDirectory}/";
     };
   };
 }
