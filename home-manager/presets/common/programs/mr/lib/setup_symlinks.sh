3f0472875356f8018a8cba44d08d0c61c8aa56f7
Romain Deville <code@romaindeville.fr>
2024-07-17 00:58:42 +0200

    🐛(preset/commont): Fix way mr setup symlink
    In order to be able to symlink multiple times the same src to different dst,
    this fix inverse way symlinks array are handle.

--- home-manager/presets/common/programs/mr/lib/setup_symlinks.sh
+++ home-manager/presets/common/programs/mr/lib/setup_symlinks.sh
@@ -2,8 +2,8 @@
 
 setup_symlinks() {
   local -n symlinks=$1
-  for src in "${!symlinks[@]}"; do
-    local dst="${symlinks[src]}"
+  for dst in "${!symlinks[@]}"; do
+    local src="${symlinks[src]}"
     if ! [[ -e ${dst} ]]; then
       _log "INFO" "Setup symlinks **${src} -> ${dst}**."
       ln -s "${src}" "${dst}"
