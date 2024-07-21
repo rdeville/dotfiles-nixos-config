c08dd9d6374284d022689e009dfb7aad65c8b76b
Romain Deville <code@romaindeville.fr>
2024-07-21 13:27:18 +0200

    ✨(preset/minimal): Upgrade zsh to work with my own flake

---
+++ home-manager/presets/minimal/programs/zsh.nix
@@ -0,0 +1,25 @@
+{...}: {
+  programs = {
+    zsh = {
+      enable = true;
+      autosuggestion = {
+        enable = true;
+      };
+      syntaxHighlighting = {
+        enable = true;
+        highlighters = [
+          "main"
+        ];
+      };
+      dotDir = ".cache/zsh";
+      initExtra = ''
+        ZSHRC_CUSTOM="$HOME/.config/zsh/zshrc"
+        if [[ -f "$ZSHRC_CUSTOM" ]]; then
+          source $ZSHRC_CUSTOM
+        else
+          echo "ERROR: File $ZSHRC_CUSTOM exists"
+        fi
+      '';
+    };
+  };
+}
