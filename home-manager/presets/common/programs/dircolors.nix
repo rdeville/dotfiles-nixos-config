653489cb786e1e48a2f932079a79d4927d96e87c
Romain Deville <code@romaindeville.fr>
2024-08-13 01:07:37 +0200

    ⬆️(hm/presets/common): Update SHA for LS_COLORS repos

--- home-manager/presets/common/programs/dircolors.nix
+++ home-manager/presets/common/programs/dircolors.nix
@@ -5,7 +5,7 @@
       extraConfig = builtins.readFile (
         builtins.fetchurl {
           url = "https://raw.githubusercontent.com/trapd00r/LS_COLORS/master/LS_COLORS";
-          sha256 = "6d48ff10173a0d9fd41f87629bdebc046828f0a627fb3e37e8ecf837d1de37a9";
+          sha256 = "1w66v5fagh7slpvh56c1ncb8vqfcb2c8j9ymcd832hlqsgpa96s5";
         }
       );
     };
