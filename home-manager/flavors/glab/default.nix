8b0259f42fcc89ebde3a9c7faa7eccce027e65f3
Romain Deville <code@romaindeville.fr>
2024-08-18 00:39:09 +0200

    ✨(hm/flavors/glab): Add glab flavors

---
+++ home-manager/flavors/glab/default.nix
@@ -0,0 +1,46 @@
+{
+  userCfg,
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  moduleCfg = userCfg.flavors.glab;
+in {
+  options = {
+    glab = {
+      enable = lib.mkEnableOption "Install glab (GitLab CLI) Related Packages";
+    };
+  };
+
+  config = lib.mkIf moduleCfg.enable {
+    home = {
+      packages = with pkgs; [
+        glab
+      ];
+    };
+
+    xdg = {
+      configFile = {
+        "glab/config.yml" = {
+          enable = with pkgs; builtins.elem glab config.home.packages;
+          text = pkgs.lib.generators.toYAML {} {
+            # What protocol to use when performing git operations. Supported values: ssh, https
+            git_protocol = "ssh";
+            # Set your desired markdown renderer style. Available options are [dark, liglabt, notty] or set a custom style. Refer to https://github.com/charmbracelet/glamour#styles
+            glamour_style = "dark";
+            # Allow glab to automatically check for updates and notify you when there are new updates
+            check_update = "false";
+          };
+        };
+        "glab/aliases.yml" = {
+          enable = with pkgs; builtins.elem glab config.home.packages;
+          text = pkgs.lib.generators.toYAML {} {
+            ci = "pipeline ci";
+            co = "mr checkout";
+          };
+        };
+      };
+    };
+  };
+}
