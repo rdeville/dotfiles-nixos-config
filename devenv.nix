680d926e0f26751df59f1080d226e4e29bb9861b
Romain Deville <code@romaindeville.fr>
2024-08-18 23:25:34 +0200

    🔨 Update devenv from DGS

--- devenv.nix
+++ devenv.nix
@@ -1,7 +1,9 @@
+# BEGIN DOTGIT-SYNC BLOCK MANAGED
 {
   pkgs ? import <nixpkgs>,
   config,
   lib,
+# BEGIN DOTGIT-SYNC BLOCK EXCLUDED NIX_DEVENV_CUSTOM
   ...
 }: let
   hm = ./scripts/hm.sh;
@@ -26,15 +28,23 @@ in {
       '';
     };
   };
+# END DOTGIT-SYNC BLOCK EXCLUDED NIX_DEVENV_CUSTOM
 
   enterShell = ''
-    CLR="\033[36m"
+    CYAN="\033[36m"
     RESET="\033[0m"
-    echo -e "$CLR"
+    echo -e "$CYAN"
     echo -e "Helper scripts you can run to make your development richer:\n"
     ${pkgs.gnused}/bin/sed -e 's| |••|g' -e 's|=| |' <<EOF | ${pkgs.util-linuxMinimal}/bin/column -t | ${pkgs.gnused}/bin/sed -e 's|••| |g'
     ${lib.generators.toKeyValue {} (lib.mapAttrs (name: value: value.description) config.scripts)}
     EOF
     echo -e "$RESET"
   '';
+
+  # Dotenv files are loading before devenv using direnv
+  dotenv = {
+    enable = false;
+    disableHint = true;
+  };
 }
+# END DOTGIT-SYNC BLOCK MANAGED
