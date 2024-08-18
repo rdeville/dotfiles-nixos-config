b7b5099f7d87c8f43e72680558a25fedf3aaeb75
Romain Deville <code@romaindeville.fr>
2024-08-18 02:42:35 +0200

    🐛(hm/flavors/glab): Update config to set chmod 0600

--- home-manager/flavors/glab/default.nix
+++ home-manager/flavors/glab/default.nix
@@ -22,7 +22,9 @@ in {
 
     xdg = {
       configFile = {
-        "glab-cli/config.yml" = {
+        # Workaround while watiing a fix allowing to set xdg chmod
+        # See: https://github.com/nix-community/home-manager/issues/3090
+        "glab-cli/HMInit_config.yml" = {
           enable = with pkgs; builtins.elem glab config.home.packages;
           text = pkgs.lib.generators.toYAML {} {
             # What protocol to use when performing git operations. Supported values: ssh, https
@@ -32,13 +34,23 @@ in {
             # Allow glab to automatically check for updates and notify you when there are new updates
             check_update = "false";
           };
+          onChange = ''
+            rm -f ${config.xdg.configHome}/glab-cli/config.yml
+            cp ${config.xdg.configHome}/glab-cli/HMInit_config.yml ${config.xdg.configHome}/glab-cli/config.yml
+            chmod 0600 ${config.xdg.configHome}/glab-cli/config.yml
+          '';
         };
-        "glab-cli/aliases.yml" = {
+        "glab-cli/HMInit_aliases.yml" = {
           enable = with pkgs; builtins.elem glab config.home.packages;
           text = pkgs.lib.generators.toYAML {} {
             ci = "pipeline ci";
             co = "mr checkout";
           };
+          onChange = ''
+            rm -f ${config.xdg.configHome}/glab-cli/aliases.yml
+            cp ${config.xdg.configHome}/glab-cli/HMInit_aliases.yml ${config.xdg.configHome}/glab-cli/aliases.yml
+            chmod 0600 ${config.xdg.configHome}/glab-cli/aliases.yml
+          '';
         };
       };
     };
