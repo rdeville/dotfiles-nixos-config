2989629ba8bea83ca32677fb1c231fbcc3d88d3f
Romain Deville <code@romaindeville.fr>
2024-10-21 13:10:06 +0200

    ✨(nixos/presets): Add generic presets/default.nix

--- nixos/presets/default.nix
+++ nixos/presets/default.nix
@@ -1,8 +1,19 @@
-{inputs, ...}: let
-  mkLib = import ../../lib {inherit inputs;};
+{hostCfg, ...}: let
+  presets = builtins.filter (item: item.value.enable) (builtins.foldl' (acc: elem:
+    [
+      {
+        name = elem;
+        value = hostCfg.presets.${elem};
+      }
+    ]
+    ++ acc) [] (builtins.attrNames hostCfg.presets));
+
+  imports = (
+    builtins.foldl' (
+      acc: elem:
+        [./${elem}] ++ acc
+    ) [] (builtins.attrNames (builtins.listToAttrs presets))
+  );
 in {
-  imports =
-    (mkLib.mkImportDir ./base)
-    ++ (mkLib.mkImportDir ./common)
-    ++ (mkLib.mkImportDir ./gui);
+  imports = imports;
 }
