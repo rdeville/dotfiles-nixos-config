41084ad9a1b817fc49de2a85951431f2fcd06b5f
Romain Deville <code@romaindeville.fr>
2024-12-03 22:17:36 +0100

    ✨(os/flavors): Add k3s flavor

---
+++ nixos/flavors/k3s/default.nix
@@ -0,0 +1,49 @@
+{
+  config,
+  hostCfg,
+  lib,
+  ...
+}: let
+  cfg = config.k3s // hostCfg.flavors.k3s;
+in {
+  options = {
+    k3s = {
+      enable = lib.mkEnableOption "Install k3s Related Packages";
+      role = lib.mkOption {
+        type = lib.types.enum [
+          "server"
+          "agent"
+          "local"
+        ];
+        default = "local";
+        example = "server";
+        description = "Set the role of the node";
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    networking = {
+      firewall = lib.mkIf (cfg.role != "local") {
+        allowedTCPPorts = [
+          6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
+          # 2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
+          # 2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
+        ];
+        allowedUDPPorts = [
+          # 8472 # k3s, flannel: required if using multi-node for inter-node networking
+        ];
+      };
+    };
+
+    services = {
+      k3s = {
+        enable = true;
+        role = if cfg.role == "local" then "server" else cfg.role;
+        extraFlags = toString [
+          # "--debug" # Optionally add additional args to k3s
+        ];
+      };
+    };
+  };
+}
