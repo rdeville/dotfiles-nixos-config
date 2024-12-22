98bc017a361528d9873b36acfaa25ea5100e05c0
Romain Deville <code@romaindeville.fr>
2024-12-22 15:18:48 +0100

    ✨ Upgrade flake.nix to new module structure

--- flake.nix
+++ flake.nix
@@ -14,16 +14,12 @@
   };
 
   inputs = {
-    # Stable Nix Packages
     nixpkgs = {
-      # url = "github:nixos/nixpkgs/nixos-24.05";
       url = "github:nixos/nixpkgs/nixos-unstable";
     };
-    # Flake Utils Lib
     utils = {
       url = "github:numtide/flake-utils";
     };
-    # Devenv to automate development environment combined with direnv
     devenv = {
       url = "github:cachix/devenv";
     };
@@ -32,41 +28,31 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     # BEGIN DOTGIT-SYNC BLOCK EXCLUDED NIX_FLAKE_INPUT
-    # Nix secret managed using sops
     sops-nix = {
       url = "github:Mic92/sops-nix/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # Home Manager, manage your Home from nix
     home-manager = {
-      # url = "github:nix-community/home-manager/release-24.05";
       url = "github:nix-community/home-manager";
-      # inputs.nixpkgs.follows = "nixpkgs-unstable";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # Unstable Nix Packages
-    # nixpkgs-unstable = {
-    #   url = "github:nixos/nixpkgs/nixos-unstable";
-    # };
-    # NixOS /HM Config
+    # My Personal Public NixOS /HM Config
     nixos = {
       url = "git+https://framagit.org/rdeville-public/dotfiles/nixos-config.git";
-      # url = "/home/rdeville/git/framagit.org/public/dotfiles/nixos/";
       inputs = {
         alejandra.follows = "alejandra";
         awesome.follows = "awesome";
         home-manager.follows = "home-manager";
         nixpkgs.follows = "nixpkgs";
-        # nixpkgs-unstable.follows = "nixpkgs";
         sops-nix.follows = "sops-nix";
         utils.follows = "utils";
       };
     };
-
     awesome = {
       url = "github:awesomeWM/awesome";
       flake = false;
     };
+
     # My personal dotfiles flakes
     awesomerc = {
       url = "git+https://framagit.org/rdeville-public/dotfiles/awesomewm.git";
@@ -125,83 +111,9 @@
         inherit system;
       };
     # BEGIN DOTGIT-SYNC BLOCK EXCLUDED NIX_FLAKE_CUSTOM_VARS
-    #
-    # Some libs
-    mkLib = inputs.nixos.homeManagerModules.mkLib {inherit (inputs.nixos) inputs;};
-    accountsLib = inputs.nixos.homeManagerModules.accountsLib;
-    hmLib = inputs.nixos.homeManagerModules.hmLib {inherit (inputs.nixos) inputs;};
-
     # This is a function that generates an attribute by calling a function you
     # pass to it, with each system as an argument
     forAllSystems = inputs.nixpkgs.lib.genAttrs allSystems;
-
-    mkHostConfig = hostname:
-      import ./configs/hosts/${hostname} {
-        inherit inputs mkLib hmLib accountsLib hostname;
-      };
-
-    hmConfig = builtins.listToAttrs (builtins.concatLists (builtins.map (
-      host: let
-        hostCfg = mkHostConfig host;
-      in
-        builtins.map (
-          user: {
-            name = "${user}@${host}";
-            value = {
-              pkgs = pkgsForSystem hostCfg.system;
-              modules = [
-                # External Modules
-                inputs.nixos.inputs.sops-nix.homeManagerModules.sops
-                # Internal Modules
-                inputs.nixos.homeManagerModules.presets
-                inputs.nixos.homeManagerModules.flavors
-                # Personnal home-manager packaged dotfiles
-                inputs.awesomerc.homeManagerModules.awesomerc
-                inputs.direnvrc.homeManagerModules.direnvrc
-                inputs.neovimrc.homeManagerModules.neovimrc
-                inputs.tmuxrc.homeManagerModules.tmuxrc
-                inputs.zshrc.homeManagerModules.shellrc
-                # Personnal packaged programs
-                inputs.dotgit-sync.homeManagerModules.dotgit-sync
-                # Local Modules
-                ./home-manager/flavors
-                ./home-manager/presets
-                ./modules/home-manager.nix
-              ];
-              extraSpecialArgs = {
-                userCfg = hostCfg.users.${user};
-                inherit mkLib;
-              };
-            };
-          }
-        )
-        (mkLib.mkListDirs ./configs/hosts/${host})
-    ) (mkLib.mkListDirs ./configs/hosts)));
-
-    nixosConfig = builtins.foldl' (acc: elem:
-      {
-        "${elem}" = {
-          specialArgs = {
-            hostCfg = mkHostConfig elem;
-            inherit inputs;
-          };
-          modules = [
-            ./configs/hosts/${elem}/configuration.nix
-            ./configs/hosts/${elem}/hardware-configuration.nix
-            inputs.nixos.nixosModules.presets
-            inputs.nixos.nixosModules.flavors
-            inputs.home-manager.nixosModules.home-manager
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                # users = hmModule (mkHostConfig elem);
-              };
-            }
-          ];
-        };
-      }
-      // acc) {} (mkLib.mkListDirs ./configs/hosts);
 
     allSystems = [
       "x86_64-linux"
@@ -240,25 +152,117 @@
       );
 
       # BEGIN DOTGIT-SYNC BLOCK EXCLUDED NIX_FLAKE_OUTPUTS_CUSTOM
+
       # CONFIGURATION
       # ========================================================================
+
       # NIXOS
       # ------------------------------------------------------------------------
-      nixosConfigurations =
-        builtins.mapAttrs (
-          _: value:
-            inputs.nixpkgs.lib.nixosSystem value
-        )
-        nixosConfig;
+      nixosConfigurations = let
+        lib =
+          inputs.nixos.homeManagerModules.lib.extend
+          (
+            final: prev: (
+              import ./lib/default.nix final
+            )
+          );
+      in
+        builtins.foldl' (acc: elem:
+          {
+            "${elem}" = inputs.nixpkgs.lib.nixosSystem {
+              modules = [
+                ./nixos
+                ./configs/hosts/${elem}/configuration.nix
+                ./configs/hosts/${elem}/hardware-configuration.nix
+                inputs.nixos.nixosModules.os
+                inputs.home-manager.nixosModules.home-manager
+                {
+                  home-manager = {
+                    useGlobalPkgs = true;
+                    useUserPackages = true;
+                    # users = {
+                    #   ${username} = {...}:{
+                    #     imports = [
+                    #       ./home-manager
+                    #     ];
+                    #   };
+                    # };
+                  };
+                }
+              ];
+              specialArgs = let
+                config = import ./configs/hosts/${elem} {};
+              in {
+                inherit inputs;
+                inherit (inputs.nixos.homeManagerModules) lib;
+                inherit (config) os;
+              };
+            };
+          }
+          // acc) {} (lib.listDirs ./configs/hosts);
 
       # HOME MANAGER
       # ------------------------------------------------------------------------
-      homeConfigurations =
-        builtins.mapAttrs (
-          _: value:
-            inputs.home-manager.lib.homeManagerConfiguration value
-        )
-        hmConfig;
+      homeConfigurations = let
+        lib =
+          inputs.nixos.homeManagerModules.lib.extend
+          (
+            final: prev: (
+              import ./lib/default.nix final
+            )
+          );
+      in
+        builtins.listToAttrs (
+          builtins.concatLists (builtins.map (
+            host: let
+              pkgs =
+                pkgsForSystem
+                (
+                  import ./configs/hosts/${host} {}
+                )
+                .os
+                .system;
+            in
+              builtins.map (
+                user: {
+                  name = "${user}@${host}";
+                  value = inputs.home-manager.lib.homeManagerConfiguration {
+                    inherit pkgs;
+                    modules = [
+                      # External Modules
+                      inputs.nixos.inputs.sops-nix.homeManagerModules.sops
+                      # Internal Modules
+                      inputs.nixos.homeManagerModules.hm
+                      # Personnal home-manager packaged dotfiles
+                      inputs.awesomerc.homeManagerModules.awesomerc
+                      inputs.direnvrc.homeManagerModules.direnvrc
+                      inputs.neovimrc.homeManagerModules.neovimrc
+                      inputs.tmuxrc.homeManagerModules.tmuxrc
+                      inputs.zshrc.homeManagerModules.shellrc
+                      # Personnal packaged programs
+                      inputs.dotgit-sync.homeManagerModules.dotgit-sync
+                      # Local Modules
+                      ./home-manager
+                      # Hack to inject config
+                      ({extraConfig, ...}: {
+                        config = extraConfig;
+                      })
+                    ];
+                    extraSpecialArgs = let
+                      cfg = import ./configs/hosts/darth-maul/rdeville {
+                        inherit lib pkgs;
+                      };
+                    in {
+                      inherit inputs lib;
+                      inherit (cfg) hm extraConfig;
+                    };
+                  };
+                }
+              )
+              (lib.listDirs ./configs/hosts/${host})
+          ) (lib.listDirs ./configs/hosts))
+        );
+
       # END DOTGIT-SYNC BLOCK EXCLUDED NIX_FLAKE_OUTPUTS_CUSTOM
     };
 }
