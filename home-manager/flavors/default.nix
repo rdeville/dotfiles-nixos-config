3e13c5aa1aa4fca8d26b68f194ed30db31ea26d6
Romain Deville <code@romaindeville.fr>
2024-12-22 13:36:04 +0100

    ✨(hm/flavors): Upgrade flavors to use modules

--- home-manager/flavors/default.nix
+++ home-manager/flavors/default.nix
@@ -1,14 +1,3 @@
-{userCfg, ...}: let
-  userFlavors =
-    if userCfg ? localFlavors
-    then userCfg.localFlavors
-    else {};
-
-  flavors = builtins.filter (
-    item: userFlavors.${item}.enable
-  ) (builtins.attrNames userFlavors);
-
-  imports = builtins.map (elem: ./${elem}) flavors;
-in {
-  imports = imports;
+{lib, ...}: {
+  imports = builtins.map (item: ./${item}) (lib.importDir ./.);
 }
