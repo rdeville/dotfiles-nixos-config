481d297375f25922c740c34f98aaff0e8ac91168
Romain Deville <code@romaindeville.fr>
2024-12-28 18:13:59 +0100

    🔨 Update devenv.nix scripts references

--- devenv.nix
+++ devenv.nix
@@ -1,11 +1,14 @@
+# BEGIN DOTGIT-SYNC BLOCK MANAGED
 {
   pkgs ? import <nixpkgs>,
   config,
   lib,
+  # BEGIN DOTGIT-SYNC BLOCK EXCLUDED NIX_DEVENV_CUSTOM
   ...
 }: let
   hm = ./scripts/hm.sh;
-  nixvm = ./scripts/nixvm.sh;
+  os = ./scripts/nixos.sh;
+  fmt = ./scripts/fmt.sh;
 in {
   packages = with pkgs; [
     sops
@@ -19,22 +22,36 @@ in {
         ${hm} "$@"
       '';
     };
-    nixvm = {
-      description = "Script allowing to run VM with nixos-shell or microvm";
+    os = {
+      description = "Script acting as alias to NixOS";
       exec = ''
-        ${nixvm}
+        ${os} "$@"
+      '';
+    };
+    format = {
+      description = "Nix format current directory";
+      exec = ''
+        ${fmt} "$@"
       '';
     };
   };
+  # END DOTGIT-SYNC BLOCK EXCLUDED NIX_DEVENV_CUSTOM
 
   enterShell = ''
-    CLR="\033[36m"
+    CYAN="\033[36m"
     RESET="\033[0m"
-    echo -e "$CLR"
+    echo -e "$CYAN"
     echo -e "Helper scripts you can run to make your development richer:\n"
     ${pkgs.gnused}/bin/sed -e 's| |••|g' -e 's|=| |' <<EOF | ${pkgs.util-linuxMinimal}/bin/column -t | ${pkgs.gnused}/bin/sed -e 's|••| |g'
     ${lib.generators.toKeyValue {} (lib.mapAttrs (name: value: value.description) config.scripts)}
     EOF
     echo -e "$RESET"
   '';
+
+  # Dotenv files are loading before devenv using direnv
+  dotenv = {
+    enable = false;
+    disableHint = true;
+  };
 }
+# END DOTGIT-SYNC BLOCK MANAGED
