55628c0183096c92e61d88b5810973b36e42dfa9
Romain Deville <code@romaindeville.fr>
2025-01-04 16:57:32 +0100

    ✨(hm/flavors): Add gimp flavor

---
+++ home-manager/flavors/gimp/default.nix
@@ -0,0 +1,27 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  name = builtins.baseNameOf ./.;
+  cfg = config.hm.flavors.${name};
+in {
+  options = {
+    hm = {
+      flavors = {
+        ${name} = {
+          enable = lib.mkEnableOption "Install ${name} Home-Manager flavor.";
+        };
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    home = {
+      packages = with pkgs; [
+        gimp
+      ];
+    };
+  };
+}
