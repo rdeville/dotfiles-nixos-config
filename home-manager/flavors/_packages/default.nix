701a76c145fe43b372f5a042789285b1e928a369
Romain Deville <code@romaindeville.fr>
2025-01-05 12:31:51 +0100

    ✨(hm/flavors): Add _packages flavors to install simple pkgs without config

---
+++ home-manager/flavors/_packages/default.nix
@@ -0,0 +1,29 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  name = builtins.baseNameOf ./.;
+  cfg = config.hm.flavors.${name};
+in {
+  options = {
+    hm = {
+      flavors = {
+        ${name} = {
+          enable = lib.mkEnableOption "Install ${name} Home-Manager flavor.";
+          pkgs = lib.mkOption {
+            type = lib.types.listOf lib.types.package;
+            description = "List of nixpkgs to install.";
+            default = [];
+          };
+        };
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    home = {
+      packages = cfg.pkgs;
+    };
+  };
+}
