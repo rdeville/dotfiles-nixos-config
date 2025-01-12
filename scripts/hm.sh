911bf28b24e98ab0ddf284540df2e2c56ecd4545
Romain Deville <code@romaindeville.fr>
2025-01-12 23:54:55 +0100

    🔨(scripts): Make hm,fmt and nixos to use _init_logger.sh and nix fmt

--- scripts/hm.sh
+++ scripts/hm.sh
@@ -7,31 +7,7 @@ SCRIPTPATH="$(
   pwd -P
 )"
 
-init_logger() {
-  local log_file="${XDG_CACHE_HOME:-${HOME}/.cache}/snippets/_log.sh"
-  local last_download_file="/tmp/_log.time"
-  local delai=14400 # 4 hours
-  # shellcheck disable=SC2155
-  local curr_time=$(date +%s)
-  local time="$((curr_time - $(cat "${last_download_file}" 2>/dev/null || echo "0")))"
-
-  if ! [[ -f "${log_file}" ]] ||
-    { [[ -f "${log_file}" ]] && [[ "${time}" -gt "${delai}" ]]; }; then
-    if ping -q -c 1 framagit.org &>/dev/null; then
-      # shellcheck disable=SC1090
-      source <(curl -s https://framagit.org/-/snippets/7183/raw/main/_get_log.sh)
-      echo "${curr_time}" >"${last_download_file}"
-    else
-      echo -e "\033[1;33m[WARNING]\033[0;33m Unable to get last logger version, will use \`echo\`.\033[0m"
-      _log() {
-        echo "$@"
-      }
-    fi
-  else
-    # shellcheck disable=SC1090
-    source <(cat "${log_file}")
-  fi
-}
+source "$(git rev-parse --show-toplevel)/scripts/_init_logger.sh"
 
 main() {
   export DEBUG_LEVEL="${DEBUG_LEVEL:-INFO}"
@@ -86,6 +62,10 @@ main() {
     --extra-experimental-features \"nix-command flakes\"\
     ${options}"
 
+  cmd_fmt="nix fmt ."
+  _log "INFO" "Formatting files with command:"
+  _log "INFO" "${cmd_fmt}"
+  eval "${cmd_fmt}"
   _log "INFO" "Running **nh home ${action}** for **${profile/-c /}** with command : "
   _log "INFO" "${cmd//  /}"
   eval "${cmd}"
