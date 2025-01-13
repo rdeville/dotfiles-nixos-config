c395e4df9ee77d37fe50ef78ffbdc27b9a5dc290
Romain Deville <code@romaindeville.fr>
2025-01-13 01:58:48 +0100

    ✨(os): Add local nixos module with home-manager support

--- nixos/default.nix
+++ nixos/default.nix
@@ -1,14 +1,45 @@
 {
-  extraConfig,
+  inputs,
   lib,
-  os,
   ...
 }: {
   imports = builtins.map (item: ./${item}) (lib.importDir ./.);
 
-  config =
-    extraConfig
-    // {
-      inherit os;
+  config = {
+    home-manager = {
+      useGlobalPkgs = true;
+      useUserPackages = true;
+      users = builtins.listToAttrs (
+        builtins.concatLists (builtins.map (
+          host:
+            builtins.map (
+              user: {
+                name = "${user}";
+                value = {
+                  imports = [
+                    # Local Modules
+                    ../configs/hosts/${host}/${user}
+                    ../home-manager
+                    # External Modules
+                    inputs.nixos.inputs.sops-nix.homeManagerModules.sops
+                    inputs.nixos.inputs.nix-index-database.hmModules.nix-index
+                    # Internal Modules
+                    inputs.nixos.homeManagerModules.hm
+                    # Personnal home-manager packaged dotfiles
+                    inputs.awesomerc.homeManagerModules.awesomerc
+                    inputs.direnvrc.homeManagerModules.direnvrc
+                    inputs.neovimrc.homeManagerModules.neovimrc
+                    inputs.tmuxrc.homeManagerModules.tmuxrc
+                    inputs.zshrc.homeManagerModules.shellrc
+                    # Personnal packaged programs
+                    inputs.dotgit-sync.homeManagerModules.dotgit-sync
+                  ];
+                };
+              }
+            )
+            (lib.listDirs ../configs/hosts/${host})
+        ) (lib.listDirs ../configs/hosts))
+      );
     };
+  };
 }
