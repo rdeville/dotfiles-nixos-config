956be55b0e876d5f7d8c95934927f637a550a5f6
Romain Deville <code@romaindeville.fr>
2025-01-19 16:49:38 +0100

    ✨(hm/flavors/_gui/wayland): Add wayland config

---
+++ home-manager/flavors/_gui/wayland/default.nix
@@ -0,0 +1,28 @@
+{lib, ...}: let
+  name = builtins.baseNameOf ../.;
+  subname = builtins.baseNameOf ./.;
+in {
+  imports = [
+    ./hyprland
+    ./hyprlock
+    ./hyprswitch
+    ./hyprexpo
+    ./rofi
+    ./waybar
+    ./wl-kbptr
+  ];
+
+  options = {
+    hm = {
+      flavors = {
+        ${name} = {
+          ${subname} = {
+            enable = lib.mkDefaultEnabledOption ''
+              Install ${name}.${subname} Home-Manager flavor.
+            '';
+          };
+        };
+      };
+    };
+  };
+}
---
+++ home-manager/flavors/_gui/wayland/hyprexpo/default.nix
@@ -0,0 +1,41 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  name = builtins.baseNameOf ../../.;
+  subname = builtins.baseNameOf ../.;
+  subsubname = builtins.baseNameOf ./.;
+  cfg = config.hm.flavors.${name}.${subname}.${subsubname};
+in {
+  options = {
+    hm = {
+      flavors = {
+        ${name} = {
+          ${subname} = {
+            ${subsubname} = {
+              enable = lib.mkOption {
+                type = lib.types.bool;
+                description = "Install ${name} Home-Manager flavor.";
+                default = config.hm.flavors.${name}.${subname}.enable;
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    wayland = {
+      windowManager = {
+        hyprland = {
+          plugins = [
+            pkgs.hyprlandPlugins.hyprexpo
+          ];
+        };
+      };
+    };
+  };
+}
