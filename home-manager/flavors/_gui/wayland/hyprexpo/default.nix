c4367bfeec3d611785f85aaf6a42f291302fa337
Romain Deville <code@romaindeville.fr>
2025-01-20 12:44:54 +0100

    🔧(hm/_gui/wayland): Update wayland.hyprexpo config

--- home-manager/flavors/_gui/wayland/hyprexpo/default.nix
+++ home-manager/flavors/_gui/wayland/hyprexpo/default.nix
@@ -34,6 +34,26 @@ in {
           plugins = [
             pkgs.hyprlandPlugins.hyprexpo
           ];
+
+          extraConfig = ''
+            plugin {
+              hyprexpo {
+                columns = 3
+                gap_size = 5
+                bg_col = rgb(111111)
+                # [center/first] [workspace] e.g. first 1 or center m+1
+                workspace_method = center current
+
+                enable_gesture = false
+              }
+            }
+          '';
+
+          settings = {
+            bind = [
+              "$mod, space, hyprexpo:expo, toggle"
+            ];
+          };
         };
       };
     };
