221cea16374dc269031dff1ee166ccb0f25182e5
Romain Deville <code@romaindeville.fr>
2025-01-23 23:03:18 +0100

    🔧(hm/_gui): Change redshift to activable module (disabled by default)

--- home-manager/flavors/_gui/redshift.nix
+++ home-manager/flavors/_gui/redshift.nix
@@ -3,32 +3,49 @@
   pkgs,
   lib,
   ...
-}: {
-  services = lib.mkIf (! config.hm.isDarwin) {
-    redshift = {
-      enable = true;
-      package = with pkgs; redshift;
-      dawnTime = "6:30-8:00";
-      duskTime = "22:00-23:30";
-      provider = "manual";
-      temperature = {
-        day = 6500;
-        night = 4000;
+}: let
+  name = builtins.baseNameOf ./.;
+
+  cfg = config.hm.flavors.${name}.redshift;
+in {
+  options = {
+    hm = {
+      flavors = {
+        ${name} = {
+          redshift = {
+            enable = lib.mkEnableOption "Enable redshift installation.";
+          };
+        };
       };
-      tray = true;
-      settings = {
-        # Global settings for redshift
-        redshift = {
-          # Set the day and night screen temperatures
-          # Disable the smooth fade between temperatures when Redshift starts and stops.
-          fade = 1;
-          # Set the screen brightness for day and nigh
-          brightness-day = 1.0;
-          brightness-night = 0.5;
-          # Set the screen gamma (for all colors, or each color channel individually)
-          gamma = 0.8;
-          # Set the adjustment-method: 'randr', 'vidmode'
-          adjustment-method = "randr";
+    };
+  };
+  config = {
+    services = {
+      redshift = {
+        enable = ! config.hm.isDarwin && cfg.enable;
+        package = with pkgs; redshift;
+        dawnTime = "6:30-8:00";
+        duskTime = "22:00-23:30";
+        provider = "manual";
+        temperature = {
+          day = 6500;
+          night = 4000;
+        };
+        tray = true;
+        settings = {
+          # Global settings for redshift
+          redshift = {
+            # Set the day and night screen temperatures
+            # Disable the smooth fade between temperatures when Redshift starts and stops.
+            fade = 1;
+            # Set the screen brightness for day and nigh
+            brightness-day = 1.0;
+            brightness-night = 0.5;
+            # Set the screen gamma (for all colors, or each color channel individually)
+            gamma = 0.8;
+            # Set the adjustment-method: 'randr', 'vidmode'
+            adjustment-method = "randr";
+          };
         };
       };
     };
