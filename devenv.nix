9aa6d29288417c5b401a2f0164793b8c77602e60
Romain Deville <code@romaindeville.fr>
2025-01-27 03:08:39 +0100

    🔨(devenv): Integrate new scripts to devenv

--- devenv.nix
+++ devenv.nix
@@ -6,35 +6,51 @@
   # BEGIN DOTGIT-SYNC BLOCK EXCLUDED NIX_DEVENV_CUSTOM
   ...
 }: let
-  hm = ./scripts/hm.sh;
-  os = ./scripts/nixos.sh;
-  fmt = ./scripts/fmt.sh;
+  scriptsDetails = {
+    gc = "Apply bulk git command";
+    hm = "Manage Home-Manager config";
+    iso = "Manage NixOS ISO config";
+    key = "Manage host and user AGE & SSH keys";
+    os = "Manage NixOS config";
+    vm = "Manage NixOS VMs config";
+    sps = "Manage .sops.yaml configuration";
+    secret = "Manage host and user secret";
+  };
+  scripts = builtins.foldl' (acc: elem:
+    {
+      "${elem}" = {
+        description = scriptsDetails.${elem};
+        exec = ''${./scripts/${elem}.sh} "$@"'';
+      };
+    }
+    // acc) {} (builtins.attrNames scriptsDetails);
 in {
   packages = with pkgs; [
     sops
     alejandra
   ];
 
-  scripts = {
-    hm = {
-      description = "Script acting as alias to Home-Manager";
-      exec = ''
-        ${hm} "$@"
-      '';
-    };
-    os = {
-      description = "Script acting as alias to NixOS";
-      exec = ''
-        ${os} "$@"
-      '';
-    };
-    format = {
-      description = "Nix format current directory";
-      exec = ''
-        ${fmt} "$@"
-      '';
-    };
-  };
+  inherit scripts;
+  # scripts = {
+  #   hm = {
+  #     description = "Script acting as alias to Home-Manager";
+  #     exec = ''
+  #       ${hm} "$@"
+  #     '';
+  #   };
+  #   os = {
+  #     description = "Script acting as alias to NixOS";
+  #     exec = ''
+  #       ${os} "$@"
+  #     '';
+  #   };
+  #   format = {
+  #     description = "Nix format current directory";
+  #     exec = ''
+  #       ${fmt} "$@"
+  #     '';
+  #   };
+  # };
   # END DOTGIT-SYNC BLOCK EXCLUDED NIX_DEVENV_CUSTOM
 
   enterShell = ''
