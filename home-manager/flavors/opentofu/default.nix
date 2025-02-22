92bdefd6d7194736d857adbff22050c69c294c40
Romain Deville <code@romaindeville.fr>
2025-02-22 19:58:40 +0100

    ✨(hm/opentofu): Update pkgs and add zsh config

--- home-manager/flavors/opentofu/default.nix
+++ home-manager/flavors/opentofu/default.nix
@@ -20,9 +20,37 @@ in {
   config = lib.mkIf cfg.enable {
     home = {
       packages = with pkgs; [
+        hclfmt
         opentofu
+        terraform-docs
       ];
     };
+
+    programs = {
+      zsh = {
+        initExtra = ''
+          autoload -U +X bashcompinit && bashcompinit
+          complete -o nospace -C "$(command -v opentofu)" opentofu
+        '';
+
+        zsh-abbr = {
+          abbreviations = {
+            to = "opentofu";
+            toa = "opentofu apply";
+            tof = "opentofu fmt";
+            toi = "opentofu init";
+            too = "opentofu output";
+            top = "opentofu plan";
+            tov = "opentofu validate";
+            tos = "opentofu state";
+            tossh = "opentofu state show";
+            tosh = "opentofu show";
+            tow = "opentofu workspace";
+          };
+        };
+      };
+    };
+
 
     xdg = {
       configFile = {
