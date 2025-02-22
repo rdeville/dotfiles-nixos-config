825b511a19dd8e000ad886c3c9edce9ecf62e20f
Romain Deville <code@romaindeville.fr>
2025-02-22 20:00:14 +0100

    ✨(os/audio): Add new audio flavors with pipewire service

---
+++ nixos/flavors/audio/default.nix
@@ -0,0 +1,30 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  name = builtins.baseNameOf ./.;
+  cfg = config.os.flavors.${name};
+in {
+  options = {
+    os = {
+      flavors = {
+        ${name} = {
+          enable = lib.mkEnableOption "Install ${name} NixOS flavor.";
+        };
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    services = {
+      pipewire = {
+        enable = true;
+        pulse = {
+          enable = true;
+        };
+      };
+    };
+  };
+}
