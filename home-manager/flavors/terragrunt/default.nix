74a269bce20ed827c7b05a63660375cc7ba873f7
Romain Deville <code@romaindeville.fr>
2025-02-23 19:03:25 +0100

    ⚡️(hm/terragrunt): Improve zsh aliases and abbr

--- home-manager/flavors/terragrunt/default.nix
+++ home-manager/flavors/terragrunt/default.nix
@@ -26,36 +26,43 @@ in {
     };
 
     programs = {
-      zsh = {
+      zsh = let
+        aliasAbbr = {
+          tg = "terragrunt";
+          tgra = "terragrunt run-all";
+        };
+      in {
         initExtra = ''
           autoload -U +X bashcompinit && bashcompinit
           complete -o nospace -C "$(command -v terragrunt)" terragrunt
         '';
 
-        zsh-abbr = {
-          abbreviations = {
-            tg = "terragrunt";
-            tga = "terragrunt apply";
-            tgf = "terragrunt fmt";
-            tgi = "terragrunt init";
-            tgo = "terragrunt output";
-            tgp = "terragrunt plan";
-            tgv = "terragrunt validate";
-            tgs = "terragrunt state";
-            tgssh = "terragrunt state show";
-            tgsh = "terragrunt show";
+        shellAliases = aliasAbbr;
 
-            tgra = "terragrunt run-all";
-            tgraa = "tgra apply";
-            tgraf = "tgra fmt";
-            tgrai = "tgra init";
-            tgrao = "tgra output";
-            tgrap = "tgra plan";
-            tgrav = "tgra validate";
-            tgras = "tgra state";
-            tgrassh = "tgra state show";
-            tgrash = "tgra show";
-          };
+        zsh-abbr = {
+          abbreviations =
+            aliasAbbr
+            // {
+              tga = "tg apply";
+              tgf = "tg fmt";
+              tgi = "tg init";
+              tgo = "tg output";
+              tgp = "tg plan";
+              tgv = "tg validate";
+              tgs = "tg state";
+              tgssh = "tg state show";
+              tgsh = "tg show";
+
+              tgraa = "tgra apply";
+              tgraf = "tgra fmt";
+              tgrai = "tgra init";
+              tgrao = "tgra output";
+              tgrap = "tgra plan";
+              tgrav = "tgra validate";
+              tgras = "tgra state";
+              tgrassh = "tgra state show";
+              tgrash = "tgra show";
+            };
         };
       };
     };
