c1056d3806784f2fd206c077ef40002ba79f3e87
Romain Deville <code@romaindeville.fr>
2025-03-24 00:50:51 +0100

    🔨(scripts): Update local inputs override scripts

--- scripts/local_inputs.sh
+++ scripts/local_inputs.sh
@@ -3,12 +3,15 @@
 unset inputs
 declare -A inputs
 
-# Dotfiles
+# Config Dotfiles
 # inputs[awesomerc]="${HOME}/git/framagit.org/public/dotfiles/awesome"
 # inputs[direnvrc]="${HOME}/git/framagit.org/public/dotfiles/direnv"
 # inputs[neovimrc]="${HOME}/git/framagit.org/public/dotfiles/neovim"
 # inputs[tmuxrc]="${HOME}/git/framagit.org/public/dotfiles/tmux"
 # inputs[nixos]="${HOME}/git/framagit.org/public/dotfiles/nixos"
+
+# Data Dotfiles
+# inputs[tmuxdata]="${HOME}/git/framagit.org/private/dotfiles/tmux"
 
 # Programs
 # inputs["dotgit-sync"]="${HOME}/git/framagit.org/public/programs/dotgit-sync/"
