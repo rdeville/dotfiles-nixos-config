344c56dc39089f96c019a1e8bfa81cac8b402128
Romain Deville <code@romaindeville.fr>
2025-04-16 01:06:53 +0200

    ♻️(machines/darth-plagueis): Refacto to use templates

--- machines/darth-plagueis/rdeville/default.nix
+++ machines/darth-plagueis/rdeville/default.nix
@@ -1,34 +1,5 @@
-{
-  lib,
-  pkgs,
-  ...
-}: let
-  username = builtins.baseNameOf ./.;
-  base = import ../base.nix;
-  default = import ../../default.nix {inherit username;};
-in {
-  home = {
-    packages = with pkgs; [
-      hclfmt
-      terraform-docs
-      terragrunt
-    ];
-  };
-
-  hm = {
-    inherit username;
-    inherit (base) hostName system isMain;
-
-    flavors = {
-      inherit (default.flavors) _core;
-
-      opentofu = {
-        enable = true;
-      };
-
-      kubernetes-client = {
-        enable = true;
-      };
-    };
-  };
+{...}: {
+  imports = [
+    ../../_templates/_users/rdeville/server.nix
+  ];
 }
