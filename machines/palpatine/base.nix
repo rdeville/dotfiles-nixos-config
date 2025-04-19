7c39c510921e7317b0a355e92896119825e7df51
Romain Deville <code@romaindeville.fr>
2025-04-19 20:55:11 +0200

    🔥(machines/palpatine): Remove useless files

--- machines/palpatine/base.nix
+++
@@ -1,7 +0,0 @@
-{
-  system = "x86_64-linux";
-  hostName = builtins.baseNameOf ./.;
-  isGui = true;
-  isMain = true;
-  isWork = true;
-}
--- machines/palpatine/romaindeville/default.nix
+++ machines/palpatine/romaindeville/default.nix
@@ -4,11 +4,11 @@
   lib,
   ...
 }: let
+  keyFile = "${config.xdg.cacheHome}/.age.key";
+
   username = builtins.baseNameOf ./.;
 
-  base = import ../base.nix;
   default = import ../../default.nix {inherit username;};
-  keyFile = "/home/rdeville/.cache/.age.key";
 in {
   imports = [
     ./hm.nix
@@ -22,7 +22,7 @@ in {
     age = {
       inherit keyFile;
     };
-    defaultSopsFile = ./rdeville.enc.yaml;
+    defaultSopsFile = ./secrets.enc.yaml;
     secrets = {
       "spotify-client-id" = {
         sopsFile = ../../../common_secrets/spotify.enc.yaml;
@@ -30,10 +30,118 @@ in {
     };
   };
 
+  nixpkgs = {
+    config = {
+      allowUnfreePredicate = pkg:
+        builtins.elem (lib.getName pkg) [
+          "terraform"
+          "zsh-abbr"
+        ];
+    };
+  };
+
+  programs = {
+    kitty = {
+      settings = {
+        macos_option_as_alt = "left";
+      };
+    };
+  };
+
   home = {
     packages = with pkgs; [
-      hclfmt
-      terragrunt
+      kind
+      slack
+      ghostscript
+      (google-cloud-sdk.withExtraComponents [
+        google-cloud-sdk.components.gke-gcloud-auth-plugin
+      ])
+      google-cloud-sql-proxy
+      ssh-to-age
+      vaultwarden
+      yubikey-manager.out
     ];
+  };
+
+  hm = {
+    inherit username;
+    system = "aarch64-darwin";
+    hostName = builtins.baseNameOf ./.;
+    isGui = true;
+    isMain = true;
+    isDarwin = true;
+    isWork = true;
+
+    flavors = {
+      _core =
+        default.flavors._core
+        // {
+          git = {
+            profiles =
+              default.flavors._core.git.profiles
+              // {
+                pro = {
+                  condition = "gitdir:${config.home.homeDirectory}/git/github.com/dougs-compta/";
+                  contents = {
+                    commit = {
+                      gpgSign = false;
+                    };
+                    push = {
+                      gpgSign = "if-asked";
+                    };
+                    tag = {
+                      forceSignAnnotated = true;
+                      gpgSign = false;
+                    };
+                    user = {
+                      name = "Romain Deville";
+                      email = "romain.deville@dougs.fr";
+                    };
+                  };
+                };
+              };
+          };
+          fastfetch = {
+            logo = {
+              source = ../../../home-manager/assets/fastfetch_logos/dougs.txt;
+              color = {
+                "1" = "light_blue";
+              };
+            };
+          };
+        };
+
+      _gui = {
+        enable = true;
+      };
+
+      gh = {
+        enable = true;
+      };
+
+      kubernetes-client = {
+        enable = true;
+      };
+
+      spotify-player = {
+        enable = true;
+        client_id_command = lib.strings.concatStringsSep " " [
+          "${pkgs.coreutils}/bin/cat"
+          "${config.xdg.configHome}/sops-nix/secrets/spotify-client-id"
+        ];
+      };
+
+      ssh-client = {
+        enable = true;
+      };
+
+      terraform = {
+        enable = true;
+      };
+
+      terragrunt = {
+        enable = true;
+      };
+    };
   };
 }
