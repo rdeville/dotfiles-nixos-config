5a251a4119a9eea918c3828b37b39988ad73af6b
Romain Deville <code@romaindeville.fr>
2025-06-18 12:35:50 +0200

    ➕(flake): Add ls-colors as input now

--- flake.nix
+++ flake.nix
@@ -25,6 +25,10 @@
       url = "github:nix-community/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    ls-colors = {
+      url = "github:trapd00r/LS_COLORS";
+      flake = false;
+    };
   };
 
   outputs = inputs @ {self, ...}: let
