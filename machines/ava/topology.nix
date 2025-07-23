a6e6ab33fd1b846b7642da43b2fea80facd5fa74
Romain Deville <code@romaindeville.fr>
2025-07-23 07:56:15 +0200

    ✨(ava): Add ava base k3s configuration and secrets

---
+++ machines/ava/topology.nix
@@ -0,0 +1,21 @@
+{
+  config,
+  lib,
+  ...
+}: {
+  topology = {
+    self = {
+      hardware = {
+        info = "TODO";
+        image = ../../assets/images/devices/acemagic-cyberpunk.png;
+      };
+
+      services = {
+        openssh = {
+          hidden = false;
+          info = lib.mkForce "";
+        };
+      };
+    };
+  };
+}
