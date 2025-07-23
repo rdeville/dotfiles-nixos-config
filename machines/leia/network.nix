3e539fc838e1f7709c0eadd4bd412273624312be
Romain Deville <code@romaindeville.fr>
2025-07-23 07:57:53 +0200

    ✨(leia): Make leia k8s ready

---
+++ machines/leia/network.nix
@@ -0,0 +1,109 @@
+{
+  self,
+  config,
+  lib,
+  ...
+}: let
+  id = 12;
+
+  k8sPorts = import ../../common/config/k8s.nix {inherit config;};
+  routerNetwork = self.nixosConfigurations.kenobi.config.os.flavors.network.networks;
+  prefix = "172.16.144";
+  wgEndpoint = "${prefix}.1";
+
+  wgNetworks = [
+    (let
+      name = "wg-private";
+    in {
+      inherit name;
+      endpoint = wgEndpoint;
+      allowInput = true;
+      allowInputConnected = true;
+      allowedTCPPorts = config.services.openssh.ports;
+      allowedIPs = routerNetwork.${name}.networkCIDR;
+      address = [
+        "172.18.144.${toString id}"
+      ];
+      routes = [
+        {
+          Destination = routerNetwork.${name}.networkCIDR;
+        }
+      ];
+    })
+    (
+      let
+        name = "wg-k8s-stg";
+      in {
+        inherit name;
+        inherit
+          (k8sPorts)
+          allowedUDPPorts
+          allowedTCPPorts
+          ;
+        activationPolicy = "up";
+        allowBidirectional = true;
+        allowInputConnected = true;
+        endpoint = wgEndpoint;
+        allowedIPs = routerNetwork.${name}.networkCIDR;
+        routes = [
+          {
+            Destination = routerNetwork.${name}.networkCIDR;
+          }
+        ];
+      }
+    )
+  ];
+in {
+  sops = {
+    secrets = builtins.foldl' (acc: elem:
+      {
+        "network/${elem.name}/private-key" = {
+          sopsFile = ./_keys/${elem.name}.private.key.enc.txt;
+          format = "binary";
+          group = config.users.users.systemd-network.group;
+          mode = "0640";
+          reloadUnits = [
+            "systemd-networkd.service"
+          ];
+        };
+      }
+      // acc) {}
+    wgNetworks;
+  };
+
+  os = {
+    flavors = {
+      network = {
+        enable = true;
+        firewall = {
+          trustedInterfaces = k8sPorts.trustedInterfaces;
+        };
+        networks =
+          {
+            enp1s0 = {
+              interface = "k8s-stg";
+              DHCP = "yes";
+              activationPolicy = "up";
+              requiredForOnline = "yes";
+              nftables = {
+                allowInputConnected = true;
+              };
+              topology = {
+                addresses = [
+                  "${prefix}.${toString id}"
+                ];
+                connections = [
+                  {
+                    to = "k8s-switch";
+                    iface = "eth2";
+                    reversed = true;
+                  }
+                ];
+              };
+            };
+          }
+          // lib.mkWgNetworkClient "kenobi" wgNetworks id config;
+      };
+    };
+  };
+}
