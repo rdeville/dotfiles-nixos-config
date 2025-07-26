5fdc8deb60c5b8b19aa3c3a09a989cc50ab21d12
Romain Deville <code@romaindeville.fr>
2025-07-26 16:21:37 +0200

    🐛(kenobi/vms): Fix nginx address related to vms

--- machines/kenobi/microvms/k8s-dev/default.nix
+++ machines/kenobi/microvms/k8s-dev/default.nix
@@ -55,11 +55,11 @@ in {
         "*.dev.tekunix.cloud" = {
           listen = [
             {
-              addr = "";
+              addr = "0.0.0.0";
               port = 80;
             }
             {
-              addr = "";
+              addr = "0.0.0.0";
               port = 443;
             }
           ];
@@ -74,6 +74,30 @@ in {
 
             access_log /var/log/nginx/kube.dev.tekunix.cloud.access.log basic;
             error_log /var/log/nginx/kube.dev.tekunix.cloud.error.log debug;
+          '';
+        };
+        "*.dev.romaindeville.xyz" = {
+          listen = [
+            {
+              addr = "0.0.0.0";
+              port = 80;
+            }
+            {
+              addr = "0.0.0.0";
+              port = 443;
+            }
+          ];
+          locations = {
+            "/" = {
+              proxyPass = "http://172.30.160.201";
+              proxyWebsockets = true;
+            };
+          };
+          extraConfig = ''
+            log_format basic '$remote_addr - - [$time_local] $protocol $status $bytes_sent $bytes_received $session_time "$upstream_addr"';
+
+            access_log /var/log/nginx/kube.dev.romaindeville.xyz.access.log basic;
+            error_log /var/log/nginx/kube.dev.romaindeville.xyz.error.log debug;
           '';
         };
       };
--- machines/kenobi/microvms/k8s-stg/default.nix
+++ machines/kenobi/microvms/k8s-stg/default.nix
@@ -55,11 +55,11 @@ in {
         "*.stg.tekunix.cloud" = {
           listen = [
             {
-              addr = "";
+              addr = "0.0.0.0";
               port = 80;
             }
             {
-              addr = "";
+              addr = "0.0.0.0";
               port = 443;
             }
           ];
@@ -75,6 +75,31 @@ in {
 
             access_log /var/log/nginx/kube.stg.tekunix.cloud.access.log basic;
             error_log /var/log/nginx/kube.stg.tekunix.cloud.error.log debug;
+          '';
+        };
+        "*.stg.romaindeville.xyz" = {
+          listen = [
+            {
+              addr = "0.0.0.0";
+              port = 80;
+            }
+            {
+              addr = "0.0.0.0";
+              port = 443;
+            }
+          ];
+          locations = {
+            "/" = {
+              proxyPass = "http://172.30.144.201";
+              proxyWebsockets = true;
+            };
+          };
+
+          extraConfig = ''
+            log_format basic '$remote_addr - - [$time_local] $protocol $status $bytes_sent $bytes_received $session_time "$upstream_addr"';
+
+            access_log /var/log/nginx/kube.stg.romaindeville.xyz.access.log basic;
+            error_log /var/log/nginx/kube.stg.romaindeville.xyz.error.log debug;
           '';
         };
       };
