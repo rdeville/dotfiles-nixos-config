a925ef3bd9a5763d8a5802a7fb4a8830ce63014f
Romain Deville <code@romaindeville.fr>
2025-08-03 13:47:51 +0200

    ♻️(luke): Refacto to use new k8s-server module

--- machines/luke/default.nix
+++ machines/luke/default.nix
@@ -1,40 +1,107 @@
-{
-  inputs,
-  config,
-  ...
-}: {
+{inputs, ...}: let
+  id = 11;
+  prefix = "172.16.144";
+in {
   imports = [
     # Templated config
     ../_templates/server.nix
-    ../_templates/k3s.nix
     # Partition map
     inputs.disko.nixosModules.disko
     ./disko.nix
-    ./topology.nix
-    ./network.nix
   ];
 
-  sops = {
-    secrets = {
-      "k8s-stg-token" = {
-        sopsFile = ../../common/secrets/k8s-stg.enc.yaml;
-      };
+  k8s-server = {
+    inherit id;
+    enable = true;
+    role = "server";
+    env = "stg";
+    hostKeyDir = ./_keys;
+    clusterInit = true;
+    clusterDomain = [
+      "kube.stg.tekunix.internal"
+    ];
+    network = {
+      debug = true;
+    };
+    wireguard = {
+      endpoint = "${prefix}.1";
     };
   };
 
   os = {
     hostName = builtins.baseNameOf ./.;
-
     flavors = {
-      k3s = {
-        role = "server";
-        disableAgent = false;
-        serverAddr = "https://kube.stg.tekunix.internal:6443";
-        tokenFile = config.sops.secrets."k8s-stg-token".path;
-        extraFlags = [
-          "--node-ip 172.30.144.11"
-          "--node-external-ip 172.30.144.11"
-        ];
+      network = {
+        enable = true;
+        networks = {
+          eth-k8s = {
+            interface = "enp3s0";
+            matchConfig = {
+              name = "enp3s*";
+            };
+            DHCP = "no";
+            activationPolicy = "up";
+            requiredForOnline = "no";
+            topology = {
+              connections = [
+                {
+                  to = "k8s-switch";
+                  iface = "eth";
+                  reversed = true;
+                }
+              ];
+            };
+          };
+
+          k8s-stg = {
+            interface = "k8s-stg";
+            DHCP = "yes";
+            activationPolicy = "up";
+            requiredForOnline = "yes";
+            nftables = {
+              allowInputConnected = true;
+              forward = {
+                inputInterfaces = [
+                  # Container go to internet via k8s-stg
+                  "lxc*"
+                ];
+                outputInterfaces = [
+                  # Response from internet through k8s-stg to cilium
+                  "cilium_host"
+                ];
+              };
+            };
+            vlan = {
+              enable = true;
+              id = 144;
+              vlanInterfaces = [
+                "eth-k8s"
+              ];
+            };
+
+            topology = {
+              addresses = [
+                "${prefix}.${toString id}"
+              ];
+              connections = [
+                {
+                  to = "kenobi";
+                  iface = "k8s-stg";
+                  reversed = true;
+                }
+              ];
+            };
+          };
+        };
+      };
+    };
+  };
+
+  topology = {
+    self = {
+      hardware = {
+        info = "Intel Celeron N3000 @ 1.04GHz, 4Go DDR3";
+        image = ../../assets/images/devices/brix.png;
       };
     };
   };
