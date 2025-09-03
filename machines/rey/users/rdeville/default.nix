ed17635ad57704b44d70dcce6abeb6668180b4f7
Romain Deville <code@romaindeville.fr>
2025-09-03 18:18:19 +0200

    🔧(rey/user/rdeville): Add monitor to hyprland config

--- machines/rey/users/rdeville/default.nix
+++ machines/rey/users/rdeville/default.nix
@@ -12,7 +12,13 @@
       hyprland = {
         extraConfig = ''
           monitor = eDP-1,1620x1080@60.0,1110x1080,1.0
-          # Dock monitor
+
+          # Dock monitor (sometimes, ID of DP changes)
+          # -----------------
+
+          monitor=DP-5,1920x1080@60.0,0x0,1.0
+          monitor=DP-6,1920x1080@60.0,1920x0,1.0
+
           monitor = DP-7,1920x1080@60.0,0x0,1.0
           monitor = DP-8,1920x1080@60.0,1920x0,1.0
         '';
