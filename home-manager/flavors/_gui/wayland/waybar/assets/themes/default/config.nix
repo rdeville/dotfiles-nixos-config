ff4564905ab47736e22b1a3deecabee93645763a
Romain Deville <code@romaindeville.fr>
2025-09-18 08:37:42 +0200

    🔧(hm/gui/wayland): Remove second from waybar clock module

--- home-manager/flavors/_gui/wayland/waybar/assets/themes/default/config.nix
+++ home-manager/flavors/_gui/wayland/waybar/assets/themes/default/config.nix
@@ -51,7 +51,7 @@ in
             };
 
             clock = {
-              format = " {:%a %e %b | %H:%M:%S}";
+              format = " {:%a %e %b | %H:%M}";
               tooltip = false;
             };
 
