07ac16d26b0ec080a774eb1bb0b23786d52ce23b
Romain Deville <code@romaindeville.fr>
2025-09-20 22:20:11 +0200

    ⚡️(rey): Improve nginx to act as TLS reverse proxy

--- machines/rey/default.nix
+++ machines/rey/default.nix
@@ -1,5 +1,6 @@
 {
   inputs,
+  config,
   lib,
   ...
 }: {
@@ -13,8 +14,33 @@
     ./disko.nix
     ./networks.nix
     ./topology.nix
-    # ./microvms
+    ./nginx
   ];
+
+  sops = {
+    secrets = {
+      "openbao/cert" = {
+        format = "binary";
+        sopsFile = ./nginx/ssl/openbao.crt.enc.asc;
+        key = "";
+        group = config.users.users.openbao.group;
+        mode = "0640";
+        reloadUnits = [
+          "openbao.service"
+        ];
+      };
+      "openbao/key" = {
+        format = "binary";
+        sopsFile = ./nginx/ssl/openbao.key.enc.asc;
+        key = "";
+        group = config.users.users.openbao.group;
+        mode = "0640";
+        reloadUnits = [
+          "openbao.service"
+        ];
+      };
+    };
+  };
 
   programs = {
     ssh = {
@@ -32,56 +58,12 @@
     };
   };
 
-  networking = {
-    hosts = {
-      "127.0.0.1" = [
-        "vault.local.tekunix.cloud"
-      ];
-      "192.168.20.3" = [
-        "kube.local.tekunix.internal"
-      ];
-      "192.168.20.4" = [
-        "argo.local.tekunix.cloud"
-      ];
-    };
-  };
-
   systemd = {
     network = {
       wait-online = {
         # Don't know why, but Rey now failed on this service
         # Moreover, rey can totally be offline
         enable = false;
-      };
-    };
-  };
-  services = {
-    nginx = {
-      enable = true;
-      virtualHosts = let
-        listen = [
-          {
-            addr = "127.0.0.1";
-            port = 80;
-          }
-        ];
-      in {
-        "localhost" = {
-          inherit listen;
-          locations = {
-            "/" = {
-              root = "/var/lib/www";
-            };
-          };
-        };
-        "vault.local.tekunix.cloud" = {
-          inherit listen;
-          locations = {
-            "/" = {
-              proxyPass = "http://127.0.0.1:8200";
-            };
-          };
-        };
       };
     };
   };
@@ -100,7 +82,8 @@
         apiAddr = "http://127.0.0.1:8200";
         listeners = [
           {
-            tlsDisable = true;
+            tlsCertFile = config.sops.secrets."openbao/cert".path;
+            tlsKeyFile = config.sops.secrets."openbao/key".path;
           }
         ];
         storage = {
---
+++ machines/rey/nginx/default.nix
@@ -0,0 +1,54 @@
+{lib, ...}: let
+  proxy = {
+    "vault.local.tekunix.cloud" = "127.0.0.1:8200";
+    "kube.local.tekunix.internal" = "192.168.20.3:443";
+    "argo.local.tekunix.cloud" = "192.168.20.4:443";
+  };
+in {
+  networking = {
+    hosts = {
+      "127.0.0.1" = builtins.attrNames proxy;
+    };
+  };
+
+  services = {
+    nginx = {
+      enable = true;
+      streamConfig = ''
+        map $ssl_preread_server_name $targetBackend {
+          ${lib.strings.concatStringsSep "\n" (builtins.map (
+          addr: "${addr} ${proxy.${addr}};"
+        ) (builtins.attrNames proxy))}
+          }
+
+        server {
+          listen 127.0.0.1:443;
+
+          proxy_connect_timeout 1s;
+          proxy_timeout 3s;
+
+          proxy_pass $targetBackend;
+          ssl_preread on;
+        }
+      '';
+
+      virtualHosts = let
+        listen = [
+          {
+            addr = "127.0.0.1";
+            port = 80;
+          }
+        ];
+      in {
+        "localhost" = {
+          inherit listen;
+          locations = {
+            "/" = {
+              root = "/var/lib/www";
+            };
+          };
+        };
+      };
+    };
+  };
+}
