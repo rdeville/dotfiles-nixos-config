0d1e4156de4e23fdc5d8c2ed13f49d27eb452c22
Romain Deville <code@romaindeville.fr>
2025-12-13 20:11:11 +0100

    ✨(darwin): Add default basic nix-darwin configuration

---
+++ darwin/options.nix
@@ -0,0 +1,41 @@
+{lib, ...}: {
+  options = {
+    os = {
+      # BASE CONFIGURATION
+      hostName = lib.mkOption {
+        type = lib.types.str;
+        description = "Hostname where config will be applied.";
+      };
+
+      parentName = lib.mkOption {
+        type = lib.types.str;
+        description = "If OS is a container or a VM, this store the name of the host OS";
+        default = "";
+      };
+
+      stateVersion = lib.mkOption {
+        type = lib.types.str;
+        description = "Version of HM to follow";
+        default = 6;
+      };
+
+      system = lib.mkOption {
+        type = lib.types.str;
+        description = "Arch system where config will be applied.";
+        default = "x86_64-linux";
+      };
+
+      timeZone = lib.mkOption {
+        type = lib.types.str;
+        description = "The timezone of the host.";
+        default = "Europe/Paris";
+      };
+
+      # BOOLEAN TO PARAMETERIZE SOME FLAVORS
+      isMain = lib.mkEnableOption "Main computer.";
+      isGui = lib.mkEnableOption "GUI computer.";
+      isProd = lib.mkEnableOption "Prod computer.";
+      isWork = lib.mkEnableOption "Work computer.";
+    };
+  };
+}
