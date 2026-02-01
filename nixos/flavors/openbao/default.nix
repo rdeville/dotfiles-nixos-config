af502e9206bfe34d3dcfd393581ec28aad4cbc16
Romain Deville <code@romaindeville.fr>
2026-02-01 22:15:04 +0100

    ⚡️(os/openbao): Rework flavor to support PG and directly setup service

--- nixos/flavors/openbao/default.nix
+++ nixos/flavors/openbao/default.nix
@@ -1,24 +1,119 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: let
   name = builtins.baseNameOf ./.;
   cfg = config.os.flavors.${name};
+
+  settingsFormat = pkgs.formats.json {};
+  pgEnvVar =
+    if cfg.storage.type == "postgresql"
+    then let
+      conn = cfg.storage.postgresql.connection;
+    in
+      builtins.foldl' (acc: elem:
+        {
+          "PG${lib.strings.toUpper elem}" = toString conn.${elem};
+        }
+        // acc) {}
+      (
+        (builtins.filter (item: conn.${item} != null)) (builtins.attrNames conn)
+      )
+    else null;
+  listener = builtins.foldl' (acc: name: let
+    listenerCfg = cfg.listeners.${name};
+  in
+    [
+      (
+        {
+          inherit (listenerCfg) type;
+          address =
+            if listenerCfg.interface != null
+            then let
+              address = builtins.elemAt config.os.flavors.network.networks.${listenerCfg.interface}.address 0;
+              ip = builtins.elemAt (lib.strings.splitString "/" address) 0;
+            in "${ip}:${toString listenerCfg.port}"
+            else "${listenerCfg.ip}:${toString listenerCfg.port}";
+          disable_unauthed_rekey_endpoints = listenerCfg.disableUnauthedRekeyEndpoints;
+          custom_response_headers = let
+            res = listenerCfg.customResponseHeaders;
+          in {
+            default = res.default // res.extraDefault;
+          };
+        }
+        // (
+          if listenerCfg.tlsDisable
+          then {
+            tls_disable = listenerCfg.tlsDisable;
+          }
+          else if listenerCfg.tlsCertFile == ""
+          then {
+            tls_acme_ca_directory = listenerCfg.tlsAcmeCaDirectory;
+            tls_acme_domains = listenerCfg.tlsAcmeDomains;
+            tls_acme_email = listenerCfg.tlsAcmeEmail;
+            tls_cert_file = listenerCfg.tlsCertFile;
+          }
+          else {
+            tls_cert_file = listenerCfg.tlsCertFile;
+            tls_key_file = listenerCfg.tlsKeyFile;
+          }
+        )
+      )
+    ]
+    ++ acc) []
+  (builtins.attrNames cfg.listeners);
+
+  settings =
+    {
+      inherit listener;
+      inherit (cfg) ui;
+      # cluster_addr = cfg.clusterAddr;
+      # api_addr = cfg.apiAddr;
+    }
+    // (
+      if cfg.storage.type == "postgresql"
+      then {
+        storage = {
+          postgresql = {
+            inherit
+              (cfg.storage.postgresql)
+              table
+              max_idle_connections
+              max_parallel
+              ha_enabled
+              ha_table
+              skip_create_table
+              max_connect_retries
+              ;
+          };
+        };
+      }
+      else {
+        storage = {
+          raft = {
+            inherit
+              (cfg.storage.raft)
+              node_id
+              path
+              ;
+          };
+        };
+      }
+    )
+    // cfg.extraSettings;
 in {
   options = {
     os = {
       flavors = {
         ${name} = {
           enable = lib.mkEnableOption "Install ${name} NixOS flavors.";
-
-          extraSettings = lib.mkOption {
-            type = lib.types.attrs;
-            description = ''
-              Extra openbao settings to add in addition to the default.
-            '';
-            default = {};
+          package = lib.mkPackageOption pkgs "openbao" {
+            example = "pkgs.openbao.override { withHsm = false; withUi = false; }";
           };
+
+          ui = lib.mkDefaultEnabledOption "Disable the OpenBao UI";
 
           clusterAddr = lib.mkOption {
             type = lib.types.nullOr lib.types.str;
@@ -42,7 +137,7 @@ in {
           };
 
           listeners = lib.mkOption {
-            type = lib.types.listOf (lib.types.submodule (import ./listener.nix));
+            type = lib.types.attrsOf (lib.types.submodule (import ./listener.nix));
             description = "Listeners configuration.";
             default = {};
           };
@@ -52,99 +147,115 @@ in {
             description = "Backend storage configuration.";
             default = {};
           };
+
+          extraSettings = lib.mkOption {
+            type = lib.types.attrs;
+            description = ''
+              Extra openbao settings to add in addition to the default.
+            '';
+            default = {};
+          };
+
+          extraArgs = lib.mkOption {
+            type = lib.types.listOf lib.types.str;
+            default = [];
+            description = ''
+              Additional arguments given to OpenBao.
+            '';
+          };
         };
       };
     };
   };
 
   config = lib.mkIf cfg.enable {
-    assertions = [
-      {
-        assertion = cfg.storage.type == "raft" && cfg.clusterAddr != null;
-        message = ''
-          If `os.flavors.openbao.storage.type` is 'raft', then
-          `os.flavors.openbao.clusterAdd` is required.
-        '';
-      }
-      {
-        assertion = cfg.storage.type == "raft" && cfg.storage.raft.node_id != null && cfg.storage.raft.path != null;
-        message = ''
-          If `os.flavors.openbao.storage.type` is 'raft', then
-          `os.flavors.openbao.storage.raft.node_id` and `os.flavors.openbao.storage.raft.path` are required.
-        '';
-      }
-    ];
+    environment = {
+      systemPackages = [
+        cfg.package
+      ];
+    };
 
-    os = {
+    users = {
+      users = {
+        openbao = {
+          # home = "/var/lib/openbao";
+          group = "openbao";
+          isSystemUser = true;
+        };
+      };
+    };
+
+    users = {
+      groups = {
+        openbao = {};
+      };
+    };
+
+    systemd = {
       services = {
         openbao = {
-          enable = true;
-          settings =
-            {
-              ui = true;
-              api_addr = cfg.apiAddr;
+          description = "OpenBao - A tool for managing secrets";
 
-              listener = builtins.foldl' (acc: elem:
-                (
-                  if elem.enable
-                  then [
-                    (
-                      {
-                        inherit (elem) type;
-                        address =
-                          if elem.interface != null
-                          then let
-                            address = builtins.elemAt config.os.flavors.network.networks.${elem.interface}.address 0;
-                            ip = builtins.elemAt (lib.strings.splitString "/" address) 0;
-                          in "${ip}:${toString elem.port}"
-                          else "${elem.ip}:${toString elem.port}";
-                        disable_unauthed_rekey_endpoints = elem.disableUnauthedRekeyEndpoints;
-                        custom_response_headers = let
-                          res =
-                            elem.customResponseHeaders;
-                        in {
-                          default = res.default // res.extraDefault;
-                        };
-                      }
-                      // (
-                        if elem.tlsDisable
-                        then {
-                          tls_disable = elem.tlsDisable;
-                        }
-                        else if elem.tlsCertFile == ""
-                        then {
-                          tls_acme_ca_directory = elem.tlsAcmeCaDirectory;
-                          tls_acme_domains = elem.tlsAcmeDomains;
-                          tls_acme_email = elem.tlsAcmeEmail;
-                          tls_cert_file = elem.tlsCertFile;
-                        }
-                        else {
-                          tls_cert_file = elem.tlsCertFile;
-                          tls_key_file = elem.tlsKeyFile;
-                        }
-                      )
-                    )
-                  ]
-                  else []
-                )
-                ++ acc) []
-              cfg.listeners;
+          wantedBy = ["multi-user.target"];
+          after = ["network.target"];
 
-              storage = let
-                inherit (cfg.storage) type;
-                backend = cfg.storage.${type};
-              in {
-                "${type}" = backend;
-              };
-            }
-            // (
-              if cfg.clusterAddr != null
-              then {
-                cluster_addr = cfg.clusterAddr;
-              }
-              else {}
-            )
-            // cfg.extraSettings;
+          restartIfChanged = false; # do not restart on "nixos-rebuild switch". It would seal the storage and disrupt the clients.
+          environment = pgEnvVar;
+
+          serviceConfig = {
+            Type = "notify";
+
+            ExecStart = lib.escapeShellArgs (
+              [
+                (lib.getExe cfg.package)
+                "server"
+                "-config"
+                (settingsFormat.generate "openbao.hcl.json" settings)
+              ]
+              ++ cfg.extraArgs
+            );
+            ExecReload = "${lib.getExe' pkgs.coreutils "kill"} -SIGHUP $MAINPID";
+
+            User = "openbao";
+            Group = "openbao";
+            DynamicUser = true;
+
+            StateDirectory = "openbao";
+            StateDirectoryMode = "0700";
+            RuntimeDirectory = "openbao";
+            RuntimeDirectoryMode = "0700";
+
+            CapabilityBoundingSet = "";
+            LimitCORE = 0;
+            LockPersonality = true;
+            MemorySwapMax = 0;
+            MemoryZSwapMax = 0;
+            PrivateUsers = true;
+            ProcSubset = "pid";
+            ProtectClock = true;
+            ProtectControlGroups = true;
+            ProtectHome = true;
+            ProtectHostname = true;
+            ProtectKernelLogs = true;
+            ProtectKernelModules = true;
+            ProtectKernelTunables = true;
+            ProtectProc = "invisible";
+            Restart = "on-failure";
+            RestrictAddressFamilies = [
+              "AF_INET"
+              "AF_INET6"
+              "AF_UNIX"
+            ];
+            RestrictNamespaces = true;
+            RestrictRealtime = true;
+            SystemCallArchitectures = "native";
+            SystemCallFilter = [
+              "@system-service"
+              "@resources"
+              "~@privileged"
+            ];
+            UMask = "0077";
+          };
         };
       };
     };
--- nixos/flavors/openbao/storage.nix
+++ nixos/flavors/openbao/storage.nix
@@ -1,27 +1,165 @@
 {lib, ...}: {
   options = {
     type = lib.mkOption {
-      type = lib.types.enum ["raft"];
+      type = lib.types.enum [
+        "raft"
+        "postgresql"
+      ];
       description = "Type of backend storage";
       default = "raft";
     };
 
-    raft = {
-      node_id = lib.mkOption {
-        type = lib.types.nullOr lib.types.str;
-        description = ''
-          The identifier for the node in the Raft cluster.
-        '';
-        default = null;
-      };
+    raft = lib.mkOption {
+      type = lib.types.submodule {
+        options = {
+          node_id = lib.mkOption {
+            type = lib.types.nullOr lib.types.str;
+            description = ''
+              The identifier for the node in the Raft cluster.
+            '';
+            default = null;
+          };
 
-      path = lib.mkOption {
-        type = lib.types.str;
-        description = ''
-          Path where raft storage will storage its data.
-        '';
-        default = "/var/lib/openbao";
+          path = lib.mkOption {
+            type = lib.types.str;
+            description = ''
+              Path where raft storage will storage its data.
+            '';
+            default = "/var/lib/openbao";
+          };
+        };
       };
+      description = "Configuration of the raft storage";
+      default = {};
+    };
+
+    postgresql = lib.mkOption {
+      type = lib.types.submodule {
+        options = {
+          connection = lib.mkOption {
+            default = {};
+            description = ''
+              default = {};
+                Set of connection parameter that will be set as environment
+                variables. See [Environment Variables](https://www.postgresql.org/docs/current/libpq-envars.html)
+                for more details.
+            '';
+            type = lib.types.submodule {
+              options = {
+                host = lib.mkOption {
+                  type = lib.types.nullOr lib.types.str;
+                  description = "Name of the host to connect to";
+                  default = null;
+                  # default = "localhost";
+                };
+
+                port = lib.mkOption {
+                  type = lib.types.nullOr lib.types.int;
+                  description = "Port number to connect to at the server host";
+                  default = null;
+
+                  # default = 5432;
+                };
+
+                database = lib.mkOption {
+                  type = lib.types.nullOr lib.types.str;
+                  description = "The database name";
+                  default = null;
+                  # default = "openbao";
+                };
+
+                user = lib.mkOption {
+                  type = lib.types.nullOr lib.types.str;
+                  description = "PostgreSQL user name to connect as";
+                  default = null;
+                  # default = "openbao";
+                };
+
+                password = lib.mkOption {
+                  type = lib.types.nullOr lib.types.str;
+                  description = ''
+                    Specify the password for the user
+                  '';
+                  default = null;
+                };
+
+                sslmode = lib.mkOption {
+                  type = lib.types.nullOr lib.types.str;
+                  description = ''                    Determines whether or with what priority a
+                                      secure SSL TCP/IP connection will be negotiated with the
+                                      server.
+                  '';
+                  default = null;
+                };
+              };
+            };
+          };
+
+          table = lib.mkOption {
+            type = lib.types.str;
+            description = ''
+              Specifies the name of the table in which to write OpenBao data. OpenBao will
+              attempt to create it if missing and skip_create_table=false (the default).
+            '';
+            default = "kv_store";
+          };
+
+          max_idle_connections = lib.mkOption {
+            type = lib.types.int;
+            description = ''
+              Sets the maximum number of connections in the idle connection pool.
+            '';
+            default = 0;
+          };
+
+          max_parallel = lib.mkOption {
+            type = lib.types.str;
+            description = ''
+              Specifies the maximum number of concurrent requests to PostgreSQL.
+            '';
+            default = "128";
+          };
+
+          ha_enabled = lib.mkOption {
+            type = lib.types.bool;
+            description = ''
+              Default not enabled, requires PostgreSQL 9.5 or later.
+            '';
+            default = false;
+          };
+
+          ha_table = lib.mkOption {
+            type = lib.types.str;
+            description = ''
+              Specifies the name of the table to use for storing High Availability
+              information. OpenBao will attempt to create it if missing and
+              skip_create_table=false (the default).
+            '';
+            default = "openbao_ha_locks";
+          };
+
+          skip_create_table = lib.mkOption {
+            type = lib.types.bool;
+            description = ''
+              When enabled, will not attempt to automatically create database tables if
+              missing. Set to true if the database user does not have the required
+              permissions; otherwise, OpenBao will fail to start.
+            '';
+            default = false;
+          };
+
+          max_connect_retries = lib.mkOption {
+            type = lib.types.int;
+            description = ''
+              Maximum number of retries to perform when waiting for the database to be
+              active.
+            '';
+            default = 1;
+          };
+        };
+      };
+      description = "Configuration of the postgresql storage";
+      default = {};
     };
   };
 }
